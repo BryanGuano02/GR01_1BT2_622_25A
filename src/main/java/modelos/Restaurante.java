c91e92578c164359f502fba0297b8eae7e92f16c
Bryan Guano <94011647+BryanGuano02@users.noreply.github.com>
2025-04-18 14:17:01 -0500

    feat: add initial logic of Restaurante

---
+++ src/main/java/modelos/Restaurante.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelos;
+
+/**
+ *
+ * @author alejo
+ */
+public class Restaurante {
+private String nombre;
+private String descripcion;
+private String horarioAtencion;
+
+	public Restaurante(String nombre, String descripcion, String horarioAtencion) {
+		this.nombre = nombre;
+		this.descripcion = descripcion;
+		this.horarioAtencion = horarioAtencion;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public String getHorarioAtencion() {
+		return horarioAtencion;
+	}
+
+	public void setHorarioAtencion(String horarioAtencion) {
+		this.horarioAtencion = horarioAtencion;
+	}
+
+
+}
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -12,8 +12,20 @@
         <title>JSP Page</title>
     </head>
     <body>
-        <h1>Hello World!</h1>
-        <% String hola = "Hola mundo";%>
-        <%= hola %>
+        <h1>Registrar Restaurante</h1>
+	<form action="ControllerRestaurante" method="POST">
+	    <fieldset>
+		<legend>Información</legend>
+		<label for="nombre">Nombre del restaurante:</label>
+		<input type="text" name="nombre" placeholder="Ej: El Buen Sabor">
+		<label for="descripcion">Descripción:</label>
+		<input type="text" name="descripcion" placeholder="Ej: Restaurante de comida rápida">
+		<label for="horario">Horario de atención:</label>
+		<input type="text" name="horario" placeholder="Ej: Lunes a Domingo de 10:00 a 22:00">
+		<input type="submit" value="Registrar nuevo restaurante">
+	</fieldset>
+	</form>
+
+	<h2>Lista de Restaurantes</h2>
     </body>
 </html>
